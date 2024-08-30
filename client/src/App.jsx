d1198b684e1ec9ba623dd086bfde5c4e73748367
ParthaSarathiRautaray1 <145197547+ParthaSarathiRautaray1@users.noreply.github.com>
2024-08-30 18:39:16 +0530

    create pages and routes for projects

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes  , Route } from "react-router-dom"
+import About from "./pages/About"
+import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
+import Signin from "./pages/Signin"
+import Projects from "./pages/Projects"
+import Dashboard from "./pages/Dashboard"
 
+export default function App(){
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <h1 className='text-3xl text-red-500'>app</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        
+      </Routes>
+      
+    </BrowserRouter>
   )
+
 }
 
-export default App
+
+
+
